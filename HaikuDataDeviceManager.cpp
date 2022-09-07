d72964c58a51ea0debd3619e7e8e39be32ca2b89
X512 <danger_mail@list.ru>
2022-09-08 06:34:20 +0900

    Clipboard copy support

--- HaikuDataDeviceManager.cpp
+++ HaikuDataDeviceManager.cpp
@@ -1,5 +1,11 @@
 #include "HaikuDataDeviceManager.h"
+#include "HaikuSeat.h"
 #include <Screen.h>
+#include <Clipboard.h>
+#include <AutoLocker.h>
+#include <AutoDeleter.h>
+
+#include <string.h>
 
 extern const struct wl_interface wl_data_device_manager_interface;
 
@@ -11,40 +17,135 @@ enum {
 
 //#pragma mark - HaikuDataSource
 
-class HaikuDataSource: public WlDataSource {
-public:
-	void HandleOffer(const char *mime_type) final;
-	void HandleDestroy() final;
-	void HandleSetActions(uint32_t dnd_actions) final;
-};
 
-void HaikuDataSource::HandleOffer(const char *mime_type)
+status_t HaikuDataSource::ReadData(std::vector<uint8> &data, const char *mimeType)
+{
+	int pipes[2];
+	pipe(pipes);
+	FileDescriptorCloser readPipe(pipes[0]);
+	FileDescriptorCloser writePipe(pipes[1]);
+	fcntl(readPipe.Get(), F_SETFD, FD_CLOEXEC);
+	fcntl(writePipe.Get(), F_SETFD, FD_CLOEXEC);
+	SendSend(mimeType, writePipe.Get());
+	writePipe.Unset();
+	data.resize(0);
+	enum {
+		bufferSize = 1024,
+	};
+	while (true) {
+		data.resize(data.size() + bufferSize);
+		size_t readLen = read(readPipe.Get(), &data[data.size() - bufferSize], bufferSize);
+		data.resize(data.size() + readLen - bufferSize);
+		if (readLen == 0) {
+			break;
+		}
+	}
+	return B_OK;
+}
+
+BMessage *HaikuDataSource::ToMessage()
+{
+	ObjectDeleter<BMessage> msg(new BMessage(B_SIMPLE_DATA));
+	for (auto &mimeType: fMimeTypes) {
+		if (mimeType == "DELETE" || mimeType == "SAVE_TARGETS") {
+			continue;
+		}
+		std::vector<uint8> data;
+		ReadData(data, mimeType.c_str());
+		msg->AddData(mimeType.c_str(), B_MIME_TYPE, &data[0], data.size());
+	}
+	return msg.Detach();
+}
+
+void HaikuDataSource::HandleOffer(const char *mimeType)
+{
+	fMimeTypes.emplace(mimeType);
+}
+
+void HaikuDataSource::HandleSetActions(uint32_t dndActions)
 {
 }
 
-void HaikuDataSource::HandleDestroy()
+
+//#pragma mark - HaikuDataOffer
+
+HaikuDataOffer *HaikuDataOffer::Create(HaikuDataDevice *dataDevice)
+{
+	HaikuDataOffer *dataOffer = new(std::nothrow) HaikuDataOffer();
+	if (dataOffer == NULL) {
+		wl_client_post_no_memory(dataDevice->Client());
+		return NULL;
+	}
+	if (!dataOffer->Init(dataDevice->Client(), wl_resource_get_version(dataDevice->ToResource()), 0)) {
+		return NULL;
+	}
+	dataDevice->SendDataOffer(dataOffer->ToResource());
+	dataOffer->SendOffer("text/plain");
+	dataOffer->SendAction(0);
+	dataOffer->SendSourceActions(7);
+	return dataOffer;
+}
+
+void HaikuDataOffer::HandleAccept(uint32_t serial, const char *mime_type)
 {
 }
 
-void HaikuDataSource::HandleSetActions(uint32_t dnd_actions)
+void HaikuDataOffer::HandleReceive(const char *mime_type, int32_t fd)
+{
+	fprintf(stderr, "HaikuDataOffer::HandleReceive\n");
+	const char buf[] = "This is a test.";
+	write(fd, buf, strlen(buf));
+	close(fd);
+}
+
+void HaikuDataOffer::HandleFinish()
+{
+}
+
+void HaikuDataOffer::HandleSetActions(uint32_t dnd_actions, uint32_t preferred_action)
 {
 }
 
 
 //#pragma mark - HaikuDataDevice
 
-class HaikuDataDevice: public WlDataDevice {
-public:
-	void HandleStartDrag(struct wl_resource *source, struct wl_resource *origin, struct wl_resource *icon, uint32_t serial) final;
-	void HandleSetSelection(struct wl_resource *source, uint32_t serial) final;
-};
 
-void HaikuDataDevice::HandleStartDrag(struct wl_resource *source, struct wl_resource *origin, struct wl_resource *icon, uint32_t serial)
+HaikuDataDevice *HaikuDataDevice::Create(struct wl_client *client, uint32_t version, uint32_t id, struct wl_resource *seat)
+{
+	HaikuDataDevice *dataDevice = new(std::nothrow) HaikuDataDevice();
+	if (dataDevice == NULL) {
+		wl_client_post_no_memory(client);
+		return NULL;
+	}
+	if (!dataDevice->Init(client, version, id)) {
+		return NULL;
+	}
+	dataDevice->fSeat = HaikuSeat::FromResource(seat);
+
+	return dataDevice;
+}
+
+void HaikuDataDevice::HandleStartDrag(struct wl_resource *_source, struct wl_resource *_origin, struct wl_resource *_icon, uint32_t serial)
 {
 }
 
-void HaikuDataDevice::HandleSetSelection(struct wl_resource *source, uint32_t serial)
+void HaikuDataDevice::HandleSetSelection(struct wl_resource *_source, uint32_t serial)
 {
+	if (_source == NULL) return;
+
+	HaikuDataSource *source = HaikuDataSource::FromResource(_source);
+
+
+	ObjectDeleter<BMessage> srcMsg(source->ToMessage());
+
+	AutoLocker<BClipboard> clipboard(be_clipboard);
+	if (!clipboard.IsLocked()) return;
+	if (clipboard.Get()->Clear() != B_OK) return;
+	BMessage* clipper = be_clipboard->Data();
+	if (clipper == NULL) return;
+	*clipper = *srcMsg.Get();
+	clipper->what = B_MIME_DATA;
+	clipboard.Get()->Commit();
 }
 
 
@@ -81,12 +182,5 @@ void HaikuDataDeviceManager::HandleCreateDataSource(uint32_t id)
 
 void HaikuDataDeviceManager::HandleGetDataDevice(uint32_t id, struct wl_resource *seat)
 {
-	HaikuDataDevice *dataDevice = new(std::nothrow) HaikuDataDevice();
-	if (dataDevice == NULL) {
-		wl_client_post_no_memory(Client());
-		return;
-	}
-	if (!dataDevice->Init(Client(), wl_resource_get_version(ToResource()), id)) {
-		return;
-	}
+	HaikuDataDevice *dataDevice = HaikuDataDevice::Create(Client(), wl_resource_get_version(ToResource()), id, seat);
 }
--- HaikuDataDeviceManager.h
+++ HaikuDataDeviceManager.h
@@ -1,6 +1,54 @@
 #pragma once
 #include "Wayland.h"
+#include <set>
+#include <string>
+#include <vector>
+#include <SupportDefs.h>
 
+
+class HaikuSeat;
+class HaikuDataDevice;
+class BMessage;
+
+class HaikuDataSource: public WlDataSource {
+private:
+	std::set<std::string> fMimeTypes;
+
+public:
+	static HaikuDataSource *FromResource(struct wl_resource *resource) {return (HaikuDataSource*)WlResource::FromResource(resource);}
+
+	std::set<std::string> &MimeTypes() {return fMimeTypes;}
+
+	status_t ReadData(std::vector<uint8> &data, const char *mimeType);
+	BMessage *ToMessage();
+
+	void HandleOffer(const char *mimeType) final;
+	void HandleSetActions(uint32_t dndActions) final;
+};
+
+class HaikuDataOffer: public WlDataOffer {
+public:
+	static HaikuDataOffer *Create(HaikuDataDevice *dataDevice);
+	static HaikuDataOffer *FromResource(struct wl_resource *resource) {return (HaikuDataOffer*)WlResource::FromResource(resource);}
+
+	void HandleAccept(uint32_t serial, const char *mime_type) final;
+	void HandleReceive(const char *mime_type, int32_t fd) final;
+	void HandleFinish() final;
+	void HandleSetActions(uint32_t dnd_actions, uint32_t preferred_action) final;
+};
+
+class HaikuDataDevice: public WlDataDevice {
+private:
+	HaikuSeat *fSeat;
+
+public:
+	static HaikuDataDevice *Create(struct wl_client *client, uint32_t version, uint32_t id, struct wl_resource *seat);
+	static HaikuDataDevice *FromResource(struct wl_resource *resource) {return (HaikuDataDevice*)WlResource::FromResource(resource);}
+	virtual ~HaikuDataDevice() = default;
+
+	void HandleStartDrag(struct wl_resource *source, struct wl_resource *origin, struct wl_resource *icon, uint32_t serial) final;
+	void HandleSetSelection(struct wl_resource *source, uint32_t serial) final;
+};
 
 class HaikuDataDeviceManager: public WlDataDeviceManager {
 private:
