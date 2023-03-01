890e1de5bc171b8f0d9d4dc2c717c40f6b2eeffa
X512 <danger_mail@list.ru>
2023-03-01 21:00:13 +0900

    HaikuXdgShell: Use DoublyLinkedList

--- HaikuXdgShell.cpp
+++ HaikuXdgShell.cpp
@@ -22,15 +22,12 @@ HaikuXdgShell *HaikuXdgShell::Create(struct wl_display *display)
 	ObjectDeleter<HaikuXdgShell> global(new(std::nothrow) HaikuXdgShell());
 	if (!global.IsSet()) return NULL;
 	if (!global->Init(display, &xdg_wm_base_interface, WM_BASE_VERSION)) return NULL;
-
-	wl_list_init(&global->clients);
-
 	return global.Detach();
 }
 
 void HaikuXdgShell::Bind(struct wl_client *wl_client, uint32_t version, uint32_t id)
 {
-	HaikuXdgWmBase *client = new(std::nothrow) HaikuXdgWmBase();
+	HaikuXdgWmBase *client = new(std::nothrow) HaikuXdgWmBase(this);
 	if (client == NULL) {
 		wl_client_post_no_memory(wl_client);
 		return;
@@ -39,27 +36,15 @@ void HaikuXdgShell::Bind(struct wl_client *wl_client, uint32_t version, uint32_t
 		return;
 	}
 
-	//wl_list_init(&client->surfaces);
-
-	client->shell = this;
-
-	wl_list_insert(&this->clients, &client->link);
+	fClients.Insert(client);
 }
 
 
 HaikuXdgWmBase::~HaikuXdgWmBase()
 {
-/*
-	struct wlr_xdg_surface *surface, *tmp = NULL;
-	wl_list_for_each_safe(surface, tmp, &client->surfaces, link) {
-		destroy_xdg_surface(surface);
-	}
+	// TODO: delete owned surfaces
 
-	if (client->ping_timer != NULL) {
-		wl_event_source_remove(client->ping_timer);
-	}
-*/
-	wl_list_remove(&link);
+	fShell->fClients.Remove(this);
 }
 
 void HaikuXdgWmBase::HandleCreatePositioner(uint32_t id)
--- HaikuXdgShell.h
+++ HaikuXdgShell.h
@@ -2,30 +2,36 @@
 
 #include "XdgShell.h"
 #include "WlGlobal.h"
+#include <private/kernel/util/DoublyLinkedList.h>
 
 struct HaikuXdgShell;
 
-class HaikuXdgShell: public WlGlocal {
-private:
-	struct wl_list clients;
-
-public:
-	static HaikuXdgShell *Create(struct wl_display *display);
-	virtual ~HaikuXdgShell() = default;
-	void Bind(struct wl_client *wl_client, uint32_t version, uint32_t id) override;
-};
 
 class HaikuXdgWmBase: public XdgWmBase {
-public:
-	HaikuXdgShell *shell;
+private:
+	HaikuXdgShell *fShell;
+	DoublyLinkedListLink<HaikuXdgWmBase> fLink;
 
-	struct wl_list link; // wlr_xdg_shell.clients
-
 public:
+	typedef DoublyLinkedList<HaikuXdgWmBase, DoublyLinkedListMemberGetLink<HaikuXdgWmBase, &HaikuXdgWmBase::fLink>> List;
+
+	HaikuXdgWmBase(HaikuXdgShell *shell): fShell(shell) {}
 	virtual ~HaikuXdgWmBase();
 	static HaikuXdgWmBase *FromResource(struct wl_resource *resource) {return (HaikuXdgWmBase*)WlResource::FromResource(resource);}
 
 	void HandleCreatePositioner(uint32_t id) override;
 	void HandleGetXdgSurface(uint32_t id, struct wl_resource *surface) override;
 	void HandlePong(uint32_t serial) override;
+};
+
+class HaikuXdgShell: public WlGlocal {
+private:
+	friend class HaikuXdgWmBase;
+
+	HaikuXdgWmBase::List fClients;
+
+public:
+	static HaikuXdgShell *Create(struct wl_display *display);
+	virtual ~HaikuXdgShell() = default;
+	void Bind(struct wl_client *wl_client, uint32_t version, uint32_t id) override;
 };
