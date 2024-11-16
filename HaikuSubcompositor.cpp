9057d63f829e96b25a13747d2db918b194831449
KENZ <KENZ.gelsoft@gmail.com>
2024-11-16 10:49:52 +0900

    Fix repainting of Firefox
    - Didn't optimize BView invalidation region yet. Will not regress WaylandViews without subsurface.

--- HaikuSubcompositor.cpp
+++ HaikuSubcompositor.cpp
@@ -22,11 +22,11 @@ void SubsurfaceHook::HandleCommit()
 {
 	HaikuSurface *surf = Base();
 	HaikuSubsurface *subsurf = surf->Subsurface();
-	AppKitPtrs::LockedPtr(surf->View())->MoveTo(subsurf->GetState().x, subsurf->GetState().y);
 
 	BBitmap *bitmap = Base()->Bitmap();
 	if (Base()->View() != NULL) {
 		auto viewLocked = AppKitPtrs::LockedPtr(Base()->View());
+		viewLocked->MoveTo(subsurf->GetState().x, subsurf->GetState().y);
 		if (bitmap != NULL) {
 			viewLocked->ResizeTo(bitmap->Bounds().Width(), bitmap->Bounds().Height());
 		}
